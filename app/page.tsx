a22af13dfe37b0e7d0cad98f3f7dba8da2aa39c1
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-02-29 14:58:33 -0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Pitch from "@/components/ui/Pitch";
 
 export default function Home() {
   return (
-    <div className="flex flex-row min-w-full items-start justify-end h-screen">
-      <Pitch />
+    <div className="grid grid-cols-4">
+      <div className="col-span-1">Test 1</div>
+      <div className="col-span-1">Test 2</div>
+      <div className="col-span-2">
+        <Pitch />
+      </div>
     </div>
   );
 }
