607c2fc569870bf71dec76c06ce18691b27e98c7
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-02-29 14:12:53 -0800

    commit

--- components/ui/Player.tsx
+++ components/ui/Player.tsx
@@ -1,77 +1,77 @@
-import { DoubleArrowDownIcon } from '@radix-ui/react-icons';
-import Image from 'next/image';
+import { DoubleArrowDownIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 
 export default function Player() {
-	return (
-		<div className="flex flex-row w-30 h-36 2xl:w-48 2xl:h-48 border rounded-sm hover:bg-yellow-100 p-2">
-			<PlayerFixtureTicker />
-			<div className="w-9/12 text-xs flex flex-col h-full items-end">
-				<div className="h-1/12">
-					<button className="text-xs w-4 h-4 rounded-sm">
-						<div className="flex flex-row justify-center">
-							<DoubleArrowDownIcon className="w-3 h-3" />
-						</div>
-					</button>
-				</div>
-				<div className="flex flex-col h-full w-full">
-					<PlayerDescription />
-					<PlayerStatsTicker />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row w-30 h-36 2xl:w-48 2xl:h-48 border rounded-sm hover:bg-yellow-100 p-2">
+      <PlayerFixtureTicker />
+      <div className="w-9/12 text-xs flex flex-col h-full items-end">
+        <div className="h-1/12">
+          <button className="text-xs w-4 h-4 rounded-sm">
+            <div className="flex flex-row justify-center">
+              <DoubleArrowDownIcon className="w-3 h-3" />
+            </div>
+          </button>
+        </div>
+        <div className="flex flex-col h-full w-full">
+          <PlayerDescription />
+          <PlayerStatsTicker />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function PlayerFixtureTicker() {
-	return (
-		<div className="w-3/12 grid grid-rows-5 text-xs tracking-tighter">
-			<div className="flex flex-col bg-red-200 text-center justify-center">
-				MUN
-			</div>
-			<div className="flex flex-col bg-green-200 text-center justify-center">
-				whu
-			</div>
-			<div className="flex flex-col bg-green-200 text-center justify-center">
-				TOT
-			</div>
-			<div className="flex flex-col bg-red-200 text-center justify-center">
-				che
-			</div>
-			<div className="flex flex-col bg-green-200 text-center justify-center">
-				MCI
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-3/12 grid grid-rows-5 text-xs tracking-tighter">
+      <div className="flex flex-col bg-red-200 text-center justify-center">
+        MUN
+      </div>
+      <div className="flex flex-col bg-green-200 text-center justify-center">
+        whu
+      </div>
+      <div className="flex flex-col bg-green-200 text-center justify-center">
+        TOT
+      </div>
+      <div className="flex flex-col bg-red-200 text-center justify-center">
+        che
+      </div>
+      <div className="flex flex-col bg-green-200 text-center justify-center">
+        MCI
+      </div>
+    </div>
+  );
 }
 
 function PlayerStatsTicker() {
-	return (
-		<div className="bg-stone-300 h-1/6 grid grid-cols-3 text-[9px] 2xl:text-xs">
-			<div className="flex flex-col text-center justify-center">
-				<div>6.5$</div>
-			</div>
-			<div className="flex flex-col text-center justify-center">
-				<div>100Pts</div>
-			</div>
-			<div className="flex flex-col text-center justify-center">
-				<div>0.56</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-stone-300 h-1/6 grid grid-cols-3 text-[9px] 2xl:text-xs">
+      <div className="flex flex-col text-center justify-center">
+        <div>6.5$</div>
+      </div>
+      <div className="flex flex-col text-center justify-center">
+        <div>100Pts</div>
+      </div>
+      <div className="flex flex-col text-center justify-center">
+        <div>0.56</div>
+      </div>
+    </div>
+  );
 }
 
 function PlayerDescription() {
-	return (
-		<div className="flex flex-col h-5/6 items-center justify-around">
-			<div className="h-4/6">
-				<Image
-					src="https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_3-110.webp"
-					alt="Player"
-					width={40}
-					height={40}
-				/>
-			</div>
-			<div className="text-xs h-1/6 font-semibold 2xl:text-sm">Saka</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col h-5/6 items-center justify-around">
+      <div className="h-4/6">
+        <Image
+          src="https://fantasy.premierleague.com/dist/img/shirts/standard/shirt_3-110.webp"
+          alt="Player"
+          width={40}
+          height={40}
+        />
+      </div>
+      <div className="text-xs h-1/6 font-semibold 2xl:text-sm">Saka</div>
+    </div>
+  );
 }
