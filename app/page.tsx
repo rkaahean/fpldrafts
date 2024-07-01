83674f706615011c9fdf1e2400e24da3a7ca3426
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-01 12:52:14 +0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen max-h-screen">
       <div className="grid grid-cols-4 gap-5">
-        <div className="flex flex-col col-span-1 max-h-screen gap-1">
+        <div className="flex flex-col col-span-1 max-h-screen gap-1 pl-2 py-2">
           <div className="h-3/4 relative overflow-scroll">
             <Selector />
           </div>
           <PlayerComparison />
         </div>
-        <div className="col-span-1 h-full">
+        <div className="col-span-1 h-full py-2">
           <div className="flex flex-col h-full gap-2">
             <Drafts />
             <Fixtures />
