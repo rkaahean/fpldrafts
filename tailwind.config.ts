719240d9624199a481d2c8a6872034f0d240f3dc
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-09 09:09:28 +0400

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
+        border: "var(--border)",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "var(--background)",
