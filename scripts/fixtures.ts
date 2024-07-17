3b0235609c7c61827c53aae47ddbd7e9ef54cbac
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-17 08:28:55 +0400

    commit

--- scripts/fixtures.ts
+++ scripts/fixtures.ts
@@ -7,20 +7,31 @@ function getData() {
     .then((res) => res.json())
     .then(async (data) => {
       return await parseFixtureData(data);
+    })
+    .then(async (data) => {
+      await prisma.fPLFixtures.createMany({
+        data,
+        skipDuplicates: true,
+      });
     });
 }
 
 async function parseFixtureData(data: any): Promise<JSONResponse> {
   // get id, team, team_code, web_name, element_type
-  const formattedPicks: JSONResponse = data.map(async (fixture: any) => {
-    const team_h = await getTeamById(
-      fixture.team_h,
-      "133e854c-8817-47a9-888e-d07bd2cd76b6"
-    );
-    const team_a = await getTeamById(
-      fixture.team_a,
-      "133e854c-8817-47a9-888e-d07bd2cd76b6"
-    );
+
+  const teams = await getPlayerTeams("133e854c-8817-47a9-888e-d07bd2cd76b6");
+  const formattedPicks: JSONResponse = data.map((fixture: any) => {
+    // const team_h = await getTeamById(
+    //   fixture.team_h,
+    //   "133e854c-8817-47a9-888e-d07bd2cd76b6"
+    // );
+    // const team_a = await getTeamById(
+    //   fixture.team_a,
+    //   "133e854c-8817-47a9-888e-d07bd2cd76b6"
+    // );
+    const [team_h] = teams.filter((team) => team.team_id == fixture.team_h);
+    const [team_a] = teams.filter((team) => team.team_id == fixture.team_a);
+
     if (!team_h || !team_a) {
       console.warn(
         `Skipping fixture with team_h: ${fixture.team_h} and team_a: ${fixture.team_a} as they don't exist in the database.`
@@ -41,21 +52,22 @@ async function parseFixtureData(data: any): Promise<JSONResponse> {
       team_a_score: fixture.team_a_score,
     };
   });
-  return Promise.all(formattedPicks);
+  return formattedPicks;
 }
 
 try {
-  getData().then(async (fixtureData) => {
-    fixtureData.map(async (data) => {
-      await prisma.fPLFixtures.upsert({
-        where: {
-          code: data.code,
-        },
-        update: data,
-        create: data,
-      });
-    });
-  });
+  // getData().then(async (fixtureData) => {
+  //   fixtureData.map(async (data) => {
+  //     await prisma.fPLFixtures.upsert({
+  //       where: {
+  //         code: data.code,
+  //       },
+  //       update: data,
+  //       create: data,
+  //     });
+  //   });
+  // });
+  getData();
   prisma.$disconnect();
 } catch (e) {
   console.error(e);
@@ -66,6 +78,14 @@ async function getTeamById(team_id: number, season_id: string) {
   return await prisma.fPLPlayerTeam.findFirst({
     where: {
       team_id,
+      season_id,
+    },
+  });
+}
+
+async function getPlayerTeams(season_id: string) {
+  return await prisma.fPLPlayerTeam.findMany({
+    where: {
       season_id,
     },
   });
