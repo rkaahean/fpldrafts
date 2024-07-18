5ec387f19c7bdf68b94c9c26c5a0a25b12c4a9e2
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-18 21:59:23 +0400

    commit

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,14 +1,38 @@
-import { signIn } from "@/auth/main";
+"use client";
+
+import { signIn } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+import GoogleIcon from "@mui/icons-material/Google";
 
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google");
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <main className="grid grid-cols-2 min-h-screen min-w-screen">
+      <div className="col-span-1">Hi</div>
+      <div className="col-span-1 w-full flex flex-col min-h-screen items-center justify-center">
+        <div className="w-1/2 flex flex-col gap-5">
+          <div>
+            <div className="text-xl font-bold">Create an account</div>
+            <div className="text-muted-foreground italic">
+              An account helps store your FPL drafts. This has nothing to do
+              with the actual FPL account.
+            </div>
+          </div>
+          {/* <form
+            action={async () => {
+              "use server";
+              await signIn("google");
+            }}
+          ></form> */}
+          <Button
+            onClick={() => signIn("google")}
+            className="bg-foreground hover:bg-foreground"
+            variant="ghost"
+          >
+            <GoogleIcon className="text-background" />
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
