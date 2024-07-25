af08e64ca6ff5450ae3a498d87e293e724f5d557
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-25 16:01:27 +0400

    commit

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,3 +1,39 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Link() {
-  return <div>Link your FPL team ID</div>;
+  return (
+    <main className="flex flex-col h-screen w-screen items-center justify-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Enter your team id.</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="teamid">Team Id</Label>
+                <Input id="teamid" placeholder="44421" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button>Submit</Button>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
--- scripts/elements.ts
+++ scripts/elements.ts
@@ -1,7 +1,6 @@
-import { FPLGameweekPlayerStats, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-type JSONResponse = Omit<FPLGameweekPlayerStats, "id">[];
 function getData() {
   const players = prisma.fPLPlayer.findMany({
     orderBy: {
@@ -59,66 +58,11 @@ function getData() {
     });
 }
 
-async function parseElementsData(data: any): Promise<JSONResponse> {
-  /**
-   * parse data for each player element and return the formatted data
-   */
-
-  return data.then((res: any) => {
-    const history = res["history"];
-    return Promise.all(
-      history.map(async (element: any) => {
-        const player = await getPlayerByElement(
-          element.element,
-          // season id
-          "dca2d9c1-d28e-4e9f-87ae-2e6b53fb7865"
-        );
-        return {
-          fpl_player_id: player?.id,
-          gameweek: element.round,
-          total_points: element.total_points,
-          goals_scored: element.goals_scored,
-          assists: element.assists,
-          expected_goals: parseFloat(element.expected_goals),
-          expected_assists: parseFloat(element.expected_assists),
-          value: element.value,
-          fixture_id: element.fixture,
-        };
-      })
-    );
-  });
-}
-
 try {
   // get all players
-  const data = getData();
-  // data.then(async (res) => {
-  //   Promise.all(res).then(async (playerData) => {
-  //     playerData.flat().map(async (data) => {
-  //       await prisma.fPLGameweekPlayerStats.upsert({
-  //         where: {
-  //           fpl_player_id_fixture_id: {
-  //             fpl_player_id: data.fpl_player_id,
-  //             fixture_id: data.fixture_id,
-  //           },
-  //         },
-  //         update: data,
-  //         create: data,
-  //       });
-  //     });
-  //   });
-  // });
+  getData();
   prisma.$disconnect();
 } catch (e) {
   console.error(e);
   prisma.$disconnect();
-}
-
-export async function getPlayerByElement(element: number, season_id: string) {
-  return await prisma.fPLPlayer.findFirst({
-    where: {
-      player_id: element,
-      season_id,
-    },
-  });
 }
