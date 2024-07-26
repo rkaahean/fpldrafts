b57b8b23c73328d11830d89d9d7199be58924772
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-26 12:40:58 +0400

    commit

--- components/navbar/main.tsx
+++ components/navbar/main.tsx
@@ -2,11 +2,12 @@
 
 import { ExitIcon } from "@radix-ui/react-icons";
 import { signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 
 export default function Navbar(props: { image: string }) {
+  const router = useRouter();
   return (
     <div className="flex flex-col h-screen py-5 items-center gap-5 pl-2">
       <Avatar>
@@ -17,7 +18,7 @@ export default function Navbar(props: { image: string }) {
         variant="link"
         onClick={() => {
           signOut({ redirect: false });
-          redirect("/landing");
+          router.push("/landing");
         }}
       >
         <ExitIcon width={20} height={20}></ExitIcon>
