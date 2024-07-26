8ef6f40d1d97fc8e1d890ed2cb2bdfe3dcaf5688
Ranjan Srinivas <ranjan3101997@gmail.com>
2024-07-26 14:22:30 +0400

    commit

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,7 +2,6 @@
 
 import { signIn } from "next-auth/react";
 
-import { auth } from "@/auth/main";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,11 +12,70 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import GoogleIcon from "@mui/icons-material/Google";
+import {
+  CalendarIcon,
+  LayoutListIcon,
+  PoundSterlingIcon,
+  SaveIcon,
+  UserRoundIcon,
+} from "lucide-react";
 
 export default function SignIn() {
+  const features = [
+    {
+      icon: <SaveIcon />,
+      title: "Save Drafts",
+      description: "Store and revisit multiple team selections",
+    },
+    {
+      icon: <PoundSterlingIcon />,
+      title: "Completely Free",
+      description: "No hidden costs or premium features",
+    },
+    {
+      icon: <UserRoundIcon />,
+      title: "Player Comparisons",
+      description: "Compare stats and form of different players",
+    },
+    {
+      icon: <LayoutListIcon />,
+      title: "Draft Comparisons",
+      description: "Analyze strengths of different team setups",
+    },
+    {
+      icon: <CalendarIcon />,
+      title: "Fixture Planning",
+      description: "Plan transfers based on upcoming matches",
+    },
+  ];
+
   return (
     <main className="grid grid-cols-2 min-h-screen min-w-screen">
-      <div className="col-span-1 bg-bgsecondary"></div>
+      <div className="col-span-1 bg-bgsecondary">
+        <div className="flex flex-col items-center justify-center min-h-screen tracking-tight gap-16">
+          <div>
+            <div className="text-3xl font-semibold">
+              Welcome to FPL Planner.
+            </div>
+            <CardDescription className="text-md">
+              A free tool to plan your FPL drafts.
+            </CardDescription>
+          </div>
+          <div className="flex flex-col gap-5">
+            {features.map((feature, index) => (
+              <div key={index} className="flex flex-row gap-5">
+                <div className="feature-icon">{feature.icon}</div>
+                <div className="feature-content flex flex-col">
+                  <h3 className="feature-title">{feature.title}</h3>
+                  <p className="feature-description text-muted-foreground">
+                    {feature.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="col-span-1 w-full flex flex-col min-h-screen items-center justify-center">
         <div className="w-1/2 flex flex-col gap-5">
           <Card className="w-[350px]">
