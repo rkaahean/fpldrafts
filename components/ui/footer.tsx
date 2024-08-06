1afdabfaa8bb38cd6b91e0e8a6c24016ad50a6c0
Ranjan Srinivas <ranjan31051997@gmail.com>
2024-08-06 14:41:28 +0400

    commit

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,12 +3,12 @@ import { EnvelopeClosedIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 export default function Footer() {
   return (
     <div className="flex flex-col w-full items-center">
-      <div className="contact text-primary flex flex-row w-full items-center justify-center gap-8">
+      <div className="contact text-primary flex flex-row w-full items-center justify-center gap-8 mb-4">
         <a href="mailto:ranjan31051997@gmail.com?subject=FPL%20Drafts%20Feedback">
-          <EnvelopeClosedIcon className="w-8 h-8" />
+          <EnvelopeClosedIcon className="w-4 h-4" />
         </a>
         <a href="https://github.com/rkaahean/fpldrafts" target="blank">
-          <GitHubLogoIcon className="w-8 h-8" />
+          <GitHubLogoIcon className="w-4 h-4" />
         </a>
       </div>
     </div>
