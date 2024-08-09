be7afea910bb98f6a900beeef734232255663423
Ranjan Srinivas <ranjan31051997@gmail.com>
2024-08-09 10:45:21 +0400

    commit

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,25 +1,37 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import { Button } from "./button";
 
 export default function Header() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; // or a loading indicator
+
   return (
     <div className="flex flex-row w-full justify-end py-2 px-4 gap-4">
       <Button variant="outline">
         <a>About</a>
       </Button>
 
-      <Button
-        onClick={async () => {
-          await signIn("google", {
-            callbackUrl: `/`,
-          });
-        }}
-        variant="outline"
-      >
-        Sign In
-      </Button>
+      {!isMobile && (
+        <Button
+          onClick={async () => {
+            await signIn("google", {
+              callbackUrl: `/`,
+            });
+          }}
+          variant="outline"
+        >
+          Sign In
+        </Button>
+      )}
     </div>
   );
 }
