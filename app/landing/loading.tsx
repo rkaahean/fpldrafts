8ef090ad22ed778dfe2f163bf9896a086c85f882
Ranjan Srinivas <ranjan31051997@gmail.com>
2024-08-10 16:20:55 +0400

    commit

--- app/landing/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-screen flex-row items-center justify-center">
-      <Loader2 className="mr-2 h-12 w-12 animate-spin" />
-    </div>
-  );
-}
