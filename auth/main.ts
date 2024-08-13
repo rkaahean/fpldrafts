6ae73ccf50d4c14aa2c0b60171ef27431116b2e7
Ranjan Srinivas <ranjan31051997@gmail.com>
2024-08-13 09:19:41 +0400

    commit

--- auth/main.ts
+++ auth/main.ts
@@ -10,6 +10,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   session: {
