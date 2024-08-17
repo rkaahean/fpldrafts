8d01398c008d65e348637130af03e833607dd8a8
Ranjan Srinivas <ranjan31051997@gmail.com>
2024-08-17 12:16:37 +0400

    commit

---
+++ prisma/migrations/20240817080908_/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[fpl_player_id,fixture_id]` on the table `FPLGameweekPlayerStats` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX IF EXISTS "FPLGameweekPlayerStats_fpl_player_id_gameweek_key";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FPLGameweekPlayerStats_fpl_player_id_fixture_id_key" ON "FPLGameweekPlayerStats"("fpl_player_id", "fixture_id");
